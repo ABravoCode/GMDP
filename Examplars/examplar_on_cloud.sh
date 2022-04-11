be66983e83c70e3b67a7358ec31efe211dead132
Jeremy <1021082837@qq.com>
2022-04-11 12:12:58 +0800

    Update

--- Examplars/examplar_on_cloud.sh
+++ Examplars/examplar_on_cloud.sh
@@ -1,1 +1,1 @@
-python3 brew_poison.py  --net ResNet18 --vruns 1 --attackiter 150 --poisonkey 2000000000 --budget 0.01 --restarts 1 --pbatch 512 --ensemble 1 --name ablation_no_aug_no_aug --save 1
+python3 brew_poison.py  --net ResNet18 --vruns 1 --attackiter 150 --poisonkey 2000000000 --budget 0.01 --restarts 1 --pbatch 512 --ensemble 1 --name ablation_no_aug_no_aug --save automl-all
