4df003b06162445282011d5ce9fc9d3a126e4ae2
Jeremy <1021082837@qq.com>
2022-04-13 21:10:33 +0800

    Update Examplars

--- Examplars/examplar_on_cloud.sh
+++ Examplars/examplar_on_cloud.sh
@@ -1,1 +1,9 @@
+ssh mist@gpu28.mistgpu.com -p $port
+cd cloud/wichers-brew/
+screen -S train
+
 python3 brew_poison.py  --net ResNet18 --vruns 1 --attackiter 120 --poisonkey 2000000000 --budget 0.01 --restarts 1 --pbatch 512 --ensemble 1 --name ablation_no_aug_no_aug --save limited
+# Success, Aprox 100min
+
+python3 brew_poison.py  --recipe bullseye --net ResNet18 --vruns 1 --attackiter 100 --poisonkey 2000000000 --budget 0.01 --restarts 1 --pbatch 512 --ensemble 1 --name ablation_no_aug_no_aug --save limited
+# Fail(?), Aprox 90min
