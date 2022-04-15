2fcfce695c9e43e82a9f56fedb3e949fd8bf640f
Jeremy <1021082837@qq.com>
2022-04-15 15:09:02 +0800

    Commit Results

--- Examplars/examplar0.sh
+++
@@ -1,6 +0,0 @@
-# 本地运行参数，pbatch大了可以继续往小了改(default 512)
-# paper_exp 5_1_1文件
-conda activate MachineLearning
-python3 brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2000000000 --budget 0.01 --restarts 1 --pbatch 32 --ensemble 1 --name ablation_no_aug_no_aug --save 1
-# --paugment --noaugment --data_aug default
-# 我MBP跑这个会着火。。
--- Examplars/examplar_on_cloud.sh
+++ Examplars/examplar_on_cloud.sh
@@ -1,4 +1,4 @@
-ssh mist@gpu28.mistgpu.com -p $port
+ssh mist@url -p $port
 cd cloud/wichers-brew/
 screen -S train
 
@@ -6,4 +6,13 @@ python3 brew_poison.py  --net ResNet18 --vruns 1 --attackiter 120 --poisonkey 20
 # Success, Aprox 100min
 
 python3 brew_poison.py  --recipe bullseye --net ResNet18 --vruns 1 --attackiter 100 --poisonkey 2000000000 --budget 0.01 --restarts 1 --pbatch 512 --ensemble 1 --name ablation_no_aug_no_aug --save limited
-# Fail(?), Aprox 90min
+# Fail, Aprox 90min
+
+python3 brew_poison.py  --net ResNet18 --vruns 1 --attackiter 100 --poisonkey 2000000000 --budget 0.01 --restarts 1 --pbatch 512 --ensemble 1 --name ablation_no_aug_no_aug --save limited
+# Success, Aprox 50min
+
+python3 brew_poison.py  --recipe poison-frogs --net ResNet18 --vruns 1 --attackiter 100 --poisonkey 2000000000 --budget 0.01 --restarts 1 --pbatch 512 --ensemble 1 --name ablation_no_aug_no_aug --save limited
+# Fail, Aprox 50min 
+
+python3 brew_poison.py  --recipe metapoison --net ResNet18 --vruns 1 --attackiter 100 --poisonkey 2000000000 --budget 0.01 --restarts 1 --pbatch 512 --ensemble 1 --name ablation_no_aug_no_aug --save limited
+# Fail, Aprox 50min 
