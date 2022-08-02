3b4b21123879c4a13ec70829e759a76e296faade
Jeremy <1021082837@qq.com>
2022-08-02 09:35:29 +0000

    删除文件 poisons

--- poisons/0413Exp1/commands.sh
+++
@@ -1,1 +0,0 @@
-python3 brew_poison.py  --recipe bullseye --net ResNet18 --vruns 1 --attackiter 100 --poisonkey 2000000000 --budget 0.01 --restarts 1 --pbatch 512 --ensemble 1 --name ablation_no_aug_no_aug --save limited
